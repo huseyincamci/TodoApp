ecdaac75afc9e2a2d3c35d348423bbff3fcae20c
Huseyin Ç <hcamci.ce@gmail.com>
2019-10-27 15:57:44 +0300

    Created Data layer and models, ApplicationDbContext.

---
+++ Todos.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Todos.Data.Models;
+
+namespace Todos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<TodoList> TodoList { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Todos.Data/Models/TodoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todos.Data.Models
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual IEnumerable<Item> Items { get; set; }
+    }
+}
