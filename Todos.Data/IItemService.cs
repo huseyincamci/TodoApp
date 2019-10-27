c7fcc3923bc06ae953aec4cc15f2e388e8b2c37e
Huseyin Ç <hcamci.ce@gmail.com>
2019-10-27 16:07:21 +0300

    Created  interfaces for services.

---
+++ Todos.Data/IItemService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todos.Data.Models;
+
+namespace Todos.Data
+{
+    public interface IItemService
+    {
+        Item GetItemById();
+        List<Item> GetAllItems();
+
+        Task CreateItem(int todoListId, Item item);
+        Task DeleteItem(int id);
+        Task MarkItemAsComplete(int id);
+    }
+}
---
+++ Todos.Data/ITodoListService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todos.Data.Models;
+
+namespace Todos.Data
+{
+    public interface ITodoListService
+    {
+        TodoList GetTodoListById(int id);
+        IEnumerable<TodoList> GetAllTodoList();
+
+        Task CreateTodoList(TodoList todoList);
+        Task DeleteTodoList(int id);
+    }
+}
