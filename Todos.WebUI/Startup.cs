8f904c97059064cbda9b177fd1b4b0f0715c5b88
Huseyin Ç <hcamci.ce@gmail.com>
2019-10-28 17:48:13 +0300

    Add first migration and configure WebUI project for database

--- Todos.WebUI/Startup.cs
+++ Todos.WebUI/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Todos.Data;
 
 namespace Todos.WebUI
 {
@@ -22,6 +19,8 @@ namespace Todos.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
